ff835ce2e3dec6db55c12a8b581403a86d2e4e53
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-21 10:54:39 -0500

    Create variablecheckfunction2.r

---
+++ variablecheckfunction2.r
@@ -0,0 +1,14 @@
+test <- function() {
+  test1 <- 1031
+  test2 <- 1029
+  if (test1 > test2) {
+    print("Test1 is greater than test2!")
+  }
+  if (test2 > test1) {
+  print("Test2 is greater than test1!")
+    }
+  if (test1 == test2) {
+    print("Both numbers are equal!")
+  }
+}
+test()
