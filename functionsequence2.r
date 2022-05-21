929ff7dc1abdb84081474bbf80e97ea43127b8a6
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-21 10:32:30 -0500

    Create functionsequence2.r

---
+++ functionsequence2.r
@@ -0,0 +1,8 @@
+# Credit to https://www.tutorialspoint.com/r/r_functions.htm for help with this
+test <- function() {
+  test1 <- 1
+  test2 <- 7
+  seqns <- seq(test1, test2)
+  print(seqns)
+}
+test()
